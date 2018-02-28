8d7fb8fe1548ae56f9ba660b74928452abe8c56e
Ingrid <ingridsu@yahoo.com>
2018-02-27 17:31:46 -0800

    change validation

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,7 +3,7 @@ class Group < ActiveRecord::Base
     has_many :joins, dependent: :destroy
     has_many :users, through: :joins
 
-    validates :name, presence: true, length: { minimum: 2 }
-    validates :description, presence: true, length: { minimum: 2 }
+    validates :name, presence: true, length: { minimum: 5 }
+    validates :description, presence: true, length: { minimum: 10 }
 
 end
